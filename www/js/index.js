4eba2ca39345e4f38e5a31200b67d15df9eef869
Fitorec <chanerec@gmail.com>
2015-11-26 07:55:22 -0600

    Agregando soporte de almacenamiento de datos en el Storage

--- www/js/index.js
+++ www/js/index.js
@@ -16,62 +16,38 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-Date.prototype.addDays = function (num_of_days) {
-	 var value = this.valueOf() + (86400000 + num_of_days);
-	 return new Date(value);
-}
 
-Date.prototype.diaStr = function () {
-	var dias = ['dom', 'lun', 'mar', 'mie', 'jue', 'vie', 'sab'];
-	return dias[this.getDay()];
-}
-
-Date.prototype.mes = function () {
-	var dias = ['ene', 'feb', 'mar', 'abr', 'may', 'jun', 'jul', 'ago', 'sep', 'oct', 'nov', 'dic'];
-	return dias[this.getMonth()];
-}
 
 
 var app = {
     // Application Constructor
     initialize: function() {
-        $('#form_datos').submit(function(event) {
-			app.generarDias($(this), event);
+        $('#formulario').submit(function(event) {
+			event.preventDefault();
+			//guardo el username
+			var username = $('#username').val();
+			window.localStorage.setItem("username", username);
+			//guardo el password
+			var password = $('#password').val();
+			window.localStorage.setItem("password", password);
+			//revisamos la existencia de datos en el Storage
+			app.revisarDatos();
 		});
+		$('#borrar_datos').click(function(even) {
+			even.preventDefault();
+			window.localStorage.clear();
+			app.revisarDatos();
+		});
+		app.revisarDatos();
     },
-    generarDias: function($el, event) {
-		event.preventDefault();
-		$.mobile.changePage('#result');
-		var f_inicio = new Date($('#f_inicio').val() + ' 00:00');
-		var f_final = new Date($('#f_final').val() + ' 00:00');
-		console.log($('#f_inicio').val());
-		console.log($('#f_final').val());
-		console.log($el);
-		if(f_inicio>=f_final) {
-			console.log('vale verga!!!');
-			return;
+    
+    revisarDatos: function($el, event) {
+		if(window.localStorage.getItem("username") != null) {
+			var username = window.localStorage.getItem("username");
+			$('#username-result').text(username);
+			$.mobile.changePage('#principal');
+		} else {
+			$.mobile.changePage('#login');
 		}
-		var contentTable = '';
-		while(f_inicio<=f_final) {
-			var dia = f_inicio.diaStr();
-			console.log($('[name='+dia+']').val());
-			if(app.isChecked(dia)) {
-				contentTable += 
-						'<tr>'
-						+  '<td>'+f_inicio.getDate()+'/'+f_inicio.mes()+'</td>'
-						+  '<td>'+ dia +'</td>'
-						+ '</tr>';
-			}
-			console.log(f_inicio);
-			f_inicio = f_inicio.addDays(1);
-		}
-		$('#t-result tbody').html(contentTable);
-	},
-	isChecked : function (name) {
-		$elem = $('[name='+name+']');
-		if($elem.length==0) {
-			return false;
-		}
-		return $elem.is(":checked");
 	}
 };
