4cfaa99bdc973a3e8573eec21f12e20aa70e8354
Fitorec <chanerec@gmail.com>
2015-12-13 19:05:05 -0600

    Revisando la estructura de la aplicación

--- www/js/index.js
+++ www/js/index.js
@@ -22,33 +22,28 @@
 function populateDB(tx) {
  tx.executeSql('DROP TABLE IF EXISTS usuarios');
  tx.executeSql('CREATE TABLE IF NOT EXISTS usuarios (id integer primary key, username text, password text)');
- tx.executeSql(
-	'INSERT INTO usuarios (username, password) VALUES (?,?)',
-	["fitorec", "secret"]
-  );
- queryDB(tx);
-}
-// form the query
-function queryDB(tx) {
-	tx.executeSql("SELECT id, username, password from usuarios;", [], querySuccess, errorCB);
 }
 
 // Display the results
 function querySuccess(tx, results) {
 	 var len = results.rows.length;
 	 alert("results.rows.length: " + results.rows.length + " [should be 2]");
+	 var contenido = '';
 	 for (var i = 0; i < len; i++) { // loop as many times as there are row results
-		 document.getElementById("login").innerHTML +=
-		 "<table><tr><td>ID = " + results.rows.item(i).id +
+		 contenido +=
+		 "<tr><td>ID = " + results.rows.item(i).id +
 		 "</td><td>username = " + results.rows.item(i).usename +
 		 "</td><td>password = " + results.rows.item(i).password +
-		 "</td></tr></table>";
+		 "</td></tr>";
 	 }
+	 $('#tabla-resultados tbody').html(contenido);
+	 $.mobile.changePage('#principal');
 }
 // Transaction error callback
 function errorCB(err) {
-console.log("Error processing SQL: " + err.code);
+	alert("Error processing SQL: " + err.code);
 }
+
 // Success error callback
 function successCB() {
 
@@ -59,29 +54,27 @@ var app = {
     // Application Constructor
     initialize: function() {
 		app.db = window.sqlitePlugin.openDatabase({name: "my.db"});
-		db.transaction(populateDB, errorCB, successCB);
-        /*$('#formulario').submit(function(event) {
+		app.db.transaction(populateDB, errorCB, successCB);
+		
+        $('#formulario').submit(function(event) {
 			event.preventDefault();
 			//guardo el username
 			var username = $('#username').val();
-			//window.localStorage.setItem("username", username);
-			window.applicationPreferences.set("username", username);
 			//guardo el password
 			var password = $('#password').val();
-			//
-			// window.localStorage.setItem("password", password);
-			window.applicationPreferences.set("password", password);
-			//revisamos la existencia de datos en el Storage
-			app.revisarDatos();
-		});
-		$('#borrar_datos').click(function(even) {
-			even.preventDefault();
-			//window.localStorage.clear();
 			//app.revisarDatos();
-			$.mobile.changePage('#login');
-			window.applicationPreferences.set("username", "");
+			app.db.transaction(function (txt){
+				tx.executeSql(
+					'INSERT INTO usuarios (username, password) VALUES (?,?)',
+					[username, password]
+				  );
+			}, errorCB, querySuccess);
 		});
-		app.revisarDatos();*/
+		$('#regresar_login').click(function(even) {
+			even.preventDefault();
+			$.mobile.changePage('#login');
+		});
+		//app.revisarDatos();
     },
 
     revisarDatos: function($el, event) {
