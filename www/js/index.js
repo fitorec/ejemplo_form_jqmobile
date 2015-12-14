497fc9312b03f4ad5bdf3ba67e87ceebb039d3a9
Fitorec <chanerec@gmail.com>
2015-12-13 23:03:03 -0600

    regresando

--- www/js/index.js
+++ www/js/index.js
@@ -6,15 +6,15 @@ function onDeviceReady() {
 
 // create table
 function populateDB(tx) {
- tx.executeSql('DROP TABLE IF EXISTS test_table');
- tx.executeSql('CREATE TABLE IF NOT EXISTS test_table (id integer primary key, data1 text, data2 integer, svgImage text)');
- tx.executeSql('INSERT INTO test_table (data1, data2, svgImage) VALUES (?,?,?)', ['test1', 100, '<svg version="1.1" x="0px" y="0px" width="100px" height="100px" viewBox="0 0 100 100" enable-background="new 0 0 100 100" xml:space="preserve"><rect x="4.815" y="4.815" fill="#039BF9" stroke="" width="100" height="100"></svg>']);
- tx.executeSql("INSERT INTO test_table (data1, data2, svgImage) VALUES (?,?,?)", ['test2', 200, '<svg version="1.1" x="0px" y="0px" width="100px" height="100px" viewBox="0 0 100 100" enable-background="new 0 0 100 100" xml:space="preserve"><rect x="4.815" y="4.815" fill="#039BF9" stroke="" width="100" height="100"></svg>']);
+ tx.executeSql('DROP TABLE IF EXISTS usuarios');
+ tx.executeSql('CREATE TABLE IF NOT EXISTS usuarios (id integer primary key, username text, password integer, svgImage text)');
+ tx.executeSql('INSERT INTO usuarios (username, password, svgImage) VALUES (?,?,?)', ['test1', 100, '<svg version="1.1" x="0px" y="0px" width="100px" height="100px" viewBox="0 0 100 100" enable-background="new 0 0 100 100" xml:space="preserve"><rect x="4.815" y="4.815" fill="#039BF9" stroke="" width="100" height="100"></svg>']);
+ tx.executeSql("INSERT INTO usuarios (username, password, svgImage) VALUES (?,?,?)", ['test2', 200, '<svg version="1.1" x="0px" y="0px" width="100px" height="100px" viewBox="0 0 100 100" enable-background="new 0 0 100 100" xml:space="preserve"><rect x="4.815" y="4.815" fill="#039BF9" stroke="" width="100" height="100"></svg>']);
  queryDB(tx);
 }
 // form the query
 function queryDB(tx) {
- tx.executeSql("SELECT id, data1, data2, svgImage from test_table;", [], querySuccess, errorCB);
+ tx.executeSql("SELECT id, username, password, svgImage from usuarios;", [], querySuccess, errorCB);
 }
 // Display the results
 function querySuccess(tx, results) {
@@ -23,8 +23,8 @@ function querySuccess(tx, results) {
  for (var i = 0; i < len; i++) { // loop as many times as there are row results
  document.getElementById("output").innerHTML +=
  "<table><tr><td>ID = " + results.rows.item(i).id + 
- "</td><td>data1 = " + results.rows.item(i).data1 + 
- "</td><td>data2 = " + results.rows.item(i).data2 + 
+ "</td><td>username = " + results.rows.item(i).username + 
+ "</td><td>password = " + results.rows.item(i).password + 
  "</td><td>svgImage = " + results.rows.item(i).svgImage + "</td></tr></table>";
  } 
 }
