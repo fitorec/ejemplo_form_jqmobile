a3374b92d6628ad1251b7c117714902ab1901161
Fitorec <chanerec@gmail.com>
2015-12-14 05:21:18 -0600

    revisando el insert

--- www/js/index.js
+++ www/js/index.js
@@ -8,18 +8,22 @@ function onDeviceReady() {
 function populateDB(tx) {
  tx.executeSql('DROP TABLE IF EXISTS usuarios');
  tx.executeSql('CREATE TABLE IF NOT EXISTS usuarios (id integer primary key, username text, password text)');
- tx.executeSql('INSERT INTO usuarios (username, password) VALUES (?,?,?)', ['test1', 'sdf']);
- tx.executeSql("INSERT INTO usuarios (username, password) VALUES (?,?,?)", ['test2', 'fsd']);
+ tx.executeSql('INSERT INTO usuarios (username, password) VALUES (?,?)', ['fitorec', 'secret']);
+ tx.executeSql("INSERT INTO usuarios (username, password) VALUES (?,?)", ['heladio', 'password']);
+ tx.executeSql("INSERT INTO usuarios (username, password) VALUES (?,?)", ['rivera', 'password']);
  queryDB(tx);
 }
+
 // form the query
 function queryDB(tx) {
  tx.executeSql("SELECT id, username, password from usuarios;", [], querySuccess, errorCB);
 }
+
 // Display the results
 function querySuccess(tx, results) {
  var len = results.rows.length;
  alert("results.rows.length: " + results.rows.length + " [should be 2]"); 
+ document.getElementById("output").innerHTML = '';
  for (var i = 0; i < len; i++) { // loop as many times as there are row results
  document.getElementById("output").innerHTML +=
  "<table><tr><td>ID = " + results.rows.item(i).id + 
