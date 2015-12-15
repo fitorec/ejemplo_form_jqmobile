faaba174c332adf5c1518d3d5688f04ab10f96af
Fitorec <chanerec@gmail.com>
2015-12-14 20:41:07 -0600

    Revisando el borrado del sistema

--- www/js/index.js
+++ www/js/index.js
@@ -1,49 +1,46 @@
-// create table
+// Borra una tabla y la vuelve a crear(truncate)
 function borrarTablaUsuarios(tx) {
- tx.executeSql('DROP TABLE IF EXISTS usuarios');
- tx.executeSql('CREATE TABLE IF NOT EXISTS usuarios (id integer primary key, username text, password text)');
- queryDB(tx);
+	tx.executeSql('DROP TABLE IF EXISTS usuarios');
+	tx.executeSql('CREATE TABLE IF NOT EXISTS usuarios (id integer primary key, username text, password text)');
+	queryDB(tx);
 }
 
-// create table
+// Registra a un usuario si este no existe.
 function registrarUsuario(tx) {
- tx.executeSql('CREATE TABLE IF NOT EXISTS usuarios (id integer primary key, username text, password text)');
- var username = $('#username').val();
- var password = $('#password').val();
- tx.executeSql('INSERT INTO usuarios (username, password) VALUES (?,?)', [username, password]);
- queryDB(tx);
+	tx.executeSql('CREATE TABLE IF NOT EXISTS usuarios (id integer primary key, username text, password text)');
+	var username = $('#username').val();
+	var password = $('#password').val();
+	tx.executeSql('INSERT INTO usuarios (username, password) VALUES (?,?)', [username, password]);
+	queryDB(tx);
 }
 
 // form the query
 function queryDB(tx) {
- tx.executeSql("SELECT id, username, password from usuarios;", [], querySuccess, errorCB);
+	tx.executeSql("SELECT id, username, password from usuarios;", [], querySuccess, errorCB);
 }
 
 // Display the results
 function querySuccess(tx, results) {
- var len = results.rows.length;
- var out = '';
- for (var i = 0; i < len; i++) {
-	 out +=
-	 "<tr><td>ID = " + results.rows.item(i).id + 
-	 "</td><td>username = " + results.rows.item(i).username + 
-	 "</td><td>password = " + results.rows.item(i).password + "</td></tr>";
- }
- $("#output tbody").html(out);
+	var len = results.rows.length;
+	var out = '';
+	for (var i = 0; i < len; i++) {
+	out +=
+	"<tr><td>ID = " + results.rows.item(i).id + 
+	"</td><td>username = " + results.rows.item(i).username + 
+	"</td><td>password = " + results.rows.item(i).password + "</td></tr>";
+	}
+	$("#output tbody").html(out);
 }
-
-// Transaction error callback
+	// Transaction error callback
 function errorCB(err) {
-console.log("Error processing SQL: " + err.code);
+	console.log("Error processing SQL: " + err.code);
 }
 // Success error callback
 function successCB() {
 }
-
-
 var app = {
-    // Application Constructor
-    initialize: function() {
+	// Application Constructor
+	initialize: function() {
 		//
 		$('#formulario').submit(function(event) {
 			event.preventDefault();
@@ -54,5 +51,5 @@ var app = {
 			event.preventDefault();
 			db.transaction(borrarTablaUsuarios, errorCB, successCB);
 		});
-    }
+	}//end initialize
 };
